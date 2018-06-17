f64b947a52b27bb9eda2e82c1a995487382307e0
htethtethtunjewel <htethtethtunhtet@gmail.com>
2018-06-17 19:04:34 +0630

    Make my personal page and example with jinja

---
+++ mypersonalpage.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,url_for
+app=Flask(__name__)
+@app.route("/")
+def hello():
+      return render_template("index1.html")
+if __name__=="__main__":
+  app.run(debug=True)
+
+
